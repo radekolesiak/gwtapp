09965d3ad22b86d44f886c5f638e162abb05b8f5
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2008-09-06 12:04:19 +0000

    gzip

--- gwtapp/src/main/java/com/cuprum/filters/GZIPResponseWrapper.java
+++ gwtapp/src/main/java/com/cuprum/filters/GZIPResponseWrapper.java
@@ -46,7 +46,9 @@ public class GZIPResponseWrapper extends HttpServletResponseWrapper {
 	}
 
 	public void flushBuffer() throws IOException {
-		stream.flush();
+		if (stream != null) {
+			stream.flush();
+		}
 	}
 
 	public ServletOutputStream getOutputStream() throws IOException {
