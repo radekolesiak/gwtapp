30de4889fcf8832b9556035af0f656449a523c29
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2008-09-08 10:32:39 +0000

    timestamp

--- gwtapp/src/main/java/com/cuprum/server/common/model/usersession/xql/CrCleanUserSessions.java
+++ gwtapp/src/main/java/com/cuprum/server/common/model/usersession/xql/CrCleanUserSessions.java
@@ -3,6 +3,7 @@ package com.cuprum.server.common.model.usersession.xql;
 import java.sql.SQLException;
 import java.util.Date;
 
+import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -14,10 +15,11 @@ import com.cuprum.web.common.client.Util;
 
 public class CrCleanUserSessions implements HibernateCallback {
 	public Query getQuery(Session session) {
-		Date now = new Date();
-		return session.createQuery("delete from UserSession where "
-				+ UserSession.DATE + " < " + now.getTime() / 1000 + " - "
-				+ Constants.USER_SESSION_TIME_OUT / 1000);
+		Query query = session.createQuery("delete from UserSession where "
+				+ UserSession.DATE + " < :timeoff");
+		query.setParameter("timeoff", new Date(new Date().getTime()
+				- Constants.USER_SESSION_TIME_OUT), Hibernate.TIMESTAMP);
+		return query;
 	}
 
 	private long lastUpdate = 0L;
