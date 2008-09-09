a9005e9db2ffb793e5c83c03d639969e132b9e15
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2008-09-09 18:32:32 +0000

    cloak

---
+++ gwtapp/src/main/resources/com/cuprum/web/smallapp/mainapp/public/cloak.php
@@ -0,0 +1,7 @@
+<html>
+<title>My Site&#39;s Title</title>
+<frameset rows="0,*" framespacing="0" border="0" frameborder="0">
+<frame scrolling="no" noresize>
+<frame scrolling="auto" noresize src="http://gwtapp.cuprum.biz:8080/gwtapp/com.cuprum.web.smallapp.mainapp.MainApp/MainApp.html?<?= $_SERVER['QUERY_STRING'] ?>">
+</frameset>
+</html>
