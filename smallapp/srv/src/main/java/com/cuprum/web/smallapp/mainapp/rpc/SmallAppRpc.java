2e9e48c7c82d5313042701357f3f9b5887906c37
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2008-10-04 17:47:46 +0000

    rpc

--- smallapp/srv/src/main/java/com/cuprum/web/smallapp/mainapp/rpc/SmallAppRpc.java
+++ smallapp/srv/src/main/java/com/cuprum/web/smallapp/mainapp/rpc/SmallAppRpc.java
@@ -1,15 +1,10 @@
 package com.cuprum.web.smallapp.mainapp.rpc;
 
-import org.apache.log4j.Logger;
-
 import com.cuprum.web.common.rpc.RemoteServiceServletSpring;
 
 public class SmallAppRpc extends RemoteServiceServletSpring {
-	private static Logger LOGGER = Logger.getLogger(SmallAppRpc.class);
-
 	/**
 	 * UID
 	 */
 	private static final long serialVersionUID = -5462277142458843488L;
-
 }
