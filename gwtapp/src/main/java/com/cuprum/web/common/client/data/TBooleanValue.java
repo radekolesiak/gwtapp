37c2ef15b3ed6803edf730e235ba3c1bd35431b5
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2008-11-30 19:40:14 +0000

    TBooleanValue

---
+++ gwtapp/src/main/java/com/cuprum/web/common/client/data/TBooleanValue.java
@@ -0,0 +1,10 @@
+package com.cuprum.web.common.client.data;
+
+public class TBooleanValue extends TValue<Boolean> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7102526267806837150L;
+
+}
