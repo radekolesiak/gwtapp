6df5479b1e2e611de2de2c9969fd7857bfacc24d
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2009-10-31 10:59:46 +0000

    rename GwtAppException to RpcException

--- gwtapp/core/src/main/java/org/gwtapp/core/client/exception/RpcException.java
+++ gwtapp/core/src/main/java/org/gwtapp/core/client/exception/RpcException.java
@@ -3,6 +3,6 @@ package org.gwtapp.core.client.exception;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 @SuppressWarnings("serial")
-public class GwtAppException extends RuntimeException implements IsSerializable {
+public class RpcException extends RuntimeException implements IsSerializable {
 
 }
