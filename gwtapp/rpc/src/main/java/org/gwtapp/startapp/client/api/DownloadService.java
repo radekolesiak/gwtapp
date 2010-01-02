691992e13a0a230dd88bc15b189d2305805a8f86
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-01-02 22:27:43 +0000

    gwtapp-download.rpc -> download.rpc

--- gwtapp/rpc/src/main/java/org/gwtapp/startapp/client/api/DownloadService.java
+++ gwtapp/rpc/src/main/java/org/gwtapp/startapp/client/api/DownloadService.java
@@ -5,7 +5,7 @@ import org.gwtapp.startapp.client.data.user.register.UserRegisterModel;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
-@RemoteServiceRelativePath("gwtapp-download.rpc")
+@RemoteServiceRelativePath("download.rpc")
 public interface DownloadService extends RemoteService {
 	void download(UserRegisterModel userRegister);
 }
