6c31068e34fa896fb32cd3cc20cff3f3e7b75dd6
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-04-06 19:04:08 +0000

    changing rpc namespace from 'client' to 'rpc'

--- gwtapp/rpc/src/main/java/org/gwtapp/startapp/client/api/DownloadServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package org.gwtapp.startapp.client.api;
-
-import org.gwtapp.startapp.client.data.user.register.UserRegisterModel;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface DownloadServiceAsync {
-	void download(UserRegisterModel userRegister, AsyncCallback<Void> callback);
-}
--- gwtapp/rpc/src/main/java/org/gwtapp/startapp/client/api/GwtAppServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package org.gwtapp.startapp.client.api;
-
-import org.gwtapp.startapp.client.data.user.register.UserRegisterModel;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface GwtAppServiceAsync {
-	void register(UserRegisterModel user, AsyncCallback<Void> callback);
-}
