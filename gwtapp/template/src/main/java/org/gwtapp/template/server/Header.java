6e1715cb2c82c7af67bf172ceb026aa53b7ff6c2
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-04-26 08:21:04 +0000

    fix Template:Header bean

--- gwtapp/template/src/main/java/org/gwtapp/template/server/Header.java
+++ gwtapp/template/src/main/java/org/gwtapp/template/server/Header.java
@@ -10,17 +10,24 @@ public class Header {
 	private HttpServletResponse response;
 
 	public void setTag(String tag) {
-		response.addHeader(Template.Header.TAG, StringUtils.defaultString(tag));
+		if (response != null) {
+			response.addHeader(Template.Header.TAG, StringUtils
+					.defaultString(tag));
+		}
 	}
 
 	public void setStyle(String style) {
-		response.addHeader(Template.Header.STYLE, StringUtils
-				.defaultString(style));
+		if (response != null) {
+			response.addHeader(Template.Header.STYLE, StringUtils
+					.defaultString(style));
+		}
 	}
 
 	public void setStyleclass(String styleClass) {
-		response.addHeader(Template.Header.STYLE_CLASS, StringUtils
-				.defaultString(styleClass));
+		if (response != null) {
+			response.addHeader(Template.Header.STYLE_CLASS, StringUtils
+					.defaultString(styleClass));
+		}
 	}
 
 	public void setResponse(HttpServletResponse response) {
