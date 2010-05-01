0a31e7c8cc44937a301ee17385c0396c748b84f0
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-05-01 22:40:21 +0000

    testing

--- gwtapp/web/src/test/java/org/gwtapp/startapp/GwtTestSuiteStartApp.java
+++ gwtapp/web/src/test/java/org/gwtapp/startapp/GwtTestSuiteStartApp.java
@@ -1,14 +1,18 @@
 package org.gwtapp.startapp;
 
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
 
 public class GwtTestSuiteStartApp extends GWTTestSuite {
-	public static Test suite() {
+	@SuppressWarnings("unchecked")
+	public static Test suite() throws ClassNotFoundException {
 		TestSuite suite = new TestSuite("Tests for a StartApp Application");
-		// suite.addTestSuite(GwtTestTemplateUtils.class);
+		Class<? extends TestCase> c = (Class<? extends TestCase>) Class
+				.forName("org.gwtapp.template.client.GwtTestTemplateUtils");
+		suite.addTestSuite(c);
 		return suite;
 	}
 }
