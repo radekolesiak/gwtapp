e8f8f68c6a1f2e11b5cb410b37e31d11d88c1538
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-05-19 12:34:36 +0000

    add feedback service

---
+++ gwtapp/srv/src/main/java/org/gwtapp/startapp/server/SendFeedback.java
@@ -0,0 +1,40 @@
+package org.gwtapp.startapp.server;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.log4j.Logger;
+
+public class SendFeedback {
+
+	private final static Logger log = Logger.getLogger(SendFeedback.class);
+
+	public void sendFeedback(String email, String feedback) {
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		try {
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress("r.olesiak@gmail.com",
+					"Radek Olesiak"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					"r.olesiak@gmail.com", "Radek Olesiak"));
+			msg.setSubject("Feedback from: " + email);
+			msg.setText(feedback);
+			Transport.send(msg);
+		} catch (UnsupportedEncodingException e) {
+			log.error("", e);
+		} catch (AddressException e) {
+			log.error("", e);
+		} catch (MessagingException e) {
+			log.error("", e);
+		}
+	}
+}
