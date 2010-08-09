060fdfe88cb9164648363b4e0d590fe9b43a068c
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-09 11:34:52 +0000

    GWTAPP-21 More test - DelegatedPanel

--- gwtapp/core/src/test/java/org/gwtapp/core/client/ui/GwtTestDelegatedPanel.java
+++ gwtapp/core/src/test/java/org/gwtapp/core/client/ui/GwtTestDelegatedPanel.java
@@ -5,21 +5,21 @@ import org.junit.Test;
 
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.TextBox;
 
 public class GwtTestDelegatedPanel extends CoreTest {
-	
+
 	class DoubleBox extends DelegatedPanel<Double, String> {
+
+		private final static double eps = 1e-3;
 
 		public final TextBox delegated = new TextBox();
 
 		public int thisCount = 0;
 		public int delegatedCount = 0;
 
-		public DoubleBox(String id) {
-			super(DOM.getElementById(id));
+		public DoubleBox() {
 			add(delegated);
 			addValueChangeHandler(new ValueChangeHandler<Double>() {
 				@Override
@@ -54,10 +54,49 @@ public class GwtTestDelegatedPanel extends CoreTest {
 		public String convertToY(Double value) {
 			return "" + value;
 		}
+		
+
+		@Override
+		public boolean isDelegatedToUpdate(Double oldValue, Double newValue) {
+			if (oldValue == null ^ newValue == null) {
+				return true;
+			} else if (oldValue == null && newValue == null) {
+				return false;
+			} else {
+				return Math.abs(oldValue - newValue) > eps;
+			}
+		}		
 	}
 
 	@Test
-	public void testDelegatedPanel() {
-		assertTrue(true);
+	public void testNullValue() {
+		DoubleBox db = new DoubleBox();
+		assertNull(db.getValue());
+		assertEquals("", db.delegated.getValue());
+		db.setValue(null);
+		assertNull(db.getValue());
+		assertEquals("null", db.delegated.getValue());
+	}
+
+	@Test
+	public void testValue1dot11() {
+		DoubleBox db = new DoubleBox();
+		double value = 1.11;
+		db.setValue(value);
+		assertEquals(value, db.getValue());
+		assertEquals("" + value, db.delegated.getValue());
+	}
+
+	@Test
+	public void testValue1dot11And2dot22() {
+		DoubleBox db = new DoubleBox();
+		double value1 = 1.11;
+		db.setValue(value1);
+		assertEquals(value1, db.getValue());
+		assertEquals("" + value1, db.delegated.getValue());
+		double value2 = 2.22;
+		db.setValue(value2);
+		assertEquals(value2, db.getValue());
+		assertEquals("" + value2, db.delegated.getValue());
 	}
 }
