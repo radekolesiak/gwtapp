ac83740057c5fd76187897193a771c8429f92f27
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-10 13:15:42 +0000

    GWTAPP-21 More test - TemplatePanel

--- gwtapp/template/src/test/java/org/gwtapp/template/client/GwtTestTemplatePanel.java
+++ gwtapp/template/src/test/java/org/gwtapp/template/client/GwtTestTemplatePanel.java
@@ -5,6 +5,12 @@ import java.util.Map;
 import org.gwtapp.template.client.ui.TemplatePanel;
 import org.junit.Test;
 
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.ui.RootPanel;
+
 public class GwtTestTemplatePanel extends TemplateTest {
 
 	private class CustomTemplateCallback implements
@@ -42,6 +48,7 @@ public class GwtTestTemplatePanel extends TemplateTest {
 		assertEquals("templatePanel", panel.getStyleName());
 		assertEquals("", panel.getElement().getInnerHTML());
 		assertNull(panel.getName());
+		assertFalse(panel.isInitValueByDeferredCommand());
 	}
 
 	@Test
@@ -57,6 +64,7 @@ public class GwtTestTemplatePanel extends TemplateTest {
 		assertEquals("templatePanel bb", panel.getStyleName());
 		assertEquals("xyz", panel.getElement().getInnerHTML());
 		assertNull(panel.getName());
+		assertFalse(panel.isInitValueByDeferredCommand());
 	}
 
 	@Test
@@ -72,5 +80,55 @@ public class GwtTestTemplatePanel extends TemplateTest {
 		p2.setName("xyz");
 		assertEquals("abc", p1.getName());
 		assertEquals("xyz", p2.getName());
+	}
+
+	@Test
+	public void testValueChangeHandler() {
+		TemplatePanel<String> panel = new TemplatePanel<String>(
+				new CustomTemplateCallback());
+		RootPanel.get().add(panel);
+		assertTrue(panel.isAttached());
+		final Value<Boolean> handled = new Value<Boolean>(false);
+		final Value<String> value = new Value<String>();
+		assertFalse(handled.get());
+		panel.addValueChangeHandler(new ValueChangeHandler<String>() {
+			@Override
+			public void onValueChange(ValueChangeEvent<String> event) {
+				handled.set(true);
+				value.set(event.getValue());
+			}
+		});
+		assertNull(panel.getValue());
+		panel.setValue("012", true);
+		assertTrue(handled.get());
+		assertEquals(value.get(), panel.getValue());
+		assertEquals("012", panel.getValue());
+	}
+
+	@Test
+	public void testFireChangeHandler() {
+		TemplatePanel<String> panel = new TemplatePanel<String>(
+				new CustomTemplateCallback());
+		RootPanel.get().add(panel);
+		final Value<Boolean> handled = new Value<Boolean>(false);
+		panel.addChangeHandler(new ChangeHandler() {
+			@Override
+			public void onChange(ChangeEvent event) {
+				handled.set(true);
+			}
+		});
+		panel.fireChangeEvent();
+		assertTrue(handled.get());
+	}
+
+	@Test
+	public void testIsInitValueByDeferredCommand() {
+		TemplatePanel<String> panel = new TemplatePanel<String>(
+				new CustomTemplateCallback());
+		assertFalse(panel.isInitValueByDeferredCommand());
+		panel.setInitValueByDeferredCommand(true);
+		assertTrue(panel.isInitValueByDeferredCommand());
+		panel.setInitValueByDeferredCommand(false);
+		assertFalse(panel.isInitValueByDeferredCommand());
 	}
 }
