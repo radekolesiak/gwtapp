5e06cfdb6d93eddb785458e4c88fa5b7050b1f1b
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-13 15:32:14 +0000

    GWTAPP-54 Add cached value for wrappers - unit tests

---
+++ gwtapp/template/src/test/java/org/gwtapp/template/client/ui/GwtTestWrapperValueCache.java
@@ -0,0 +1,11 @@
+package org.gwtapp.template.client.ui;
+
+import org.gwtapp.template.client.TemplateTest;
+import org.junit.Test;
+
+public class GwtTestWrapperValueCache extends TemplateTest {
+	
+	@Test
+	public void test() {
+	}
+}
