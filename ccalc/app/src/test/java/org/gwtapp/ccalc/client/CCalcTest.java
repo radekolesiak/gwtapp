8a704363d2bc8d24414dd5dbb640505a54b6f070
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-15 14:14:21 +0000

    CCALC-25 User registering service - init, tests

---
+++ ccalc/app/src/test/java/org/gwtapp/ccalc/client/CCalcTest.java
@@ -0,0 +1,12 @@
+package org.gwtapp.ccalc.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public abstract class CCalcTest extends GWTTestCase {
+
+	@Override
+	public String getModuleName() {
+		return "org.gwtapp.ccalc.CCalcTest";
+	}
+
+}
---
+++ ccalc/app/src/test/java/org/gwtapp/ccalc/client/GwtTestUser.java
@@ -0,0 +1,11 @@
+package org.gwtapp.ccalc.client;
+
+import org.junit.Test;
+
+public class GwtTestUser extends CCalcTest {
+
+	@Test
+	public void testUserService() {
+		assertTrue(true);
+	}
+}
