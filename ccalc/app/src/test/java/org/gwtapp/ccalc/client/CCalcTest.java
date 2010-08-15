2c93ba1a1e66a570e3d07b73947bbe466d6990c7
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-15 17:50:49 +0000

    CCALC-25 User registering service - tests

--- ccalc/app/src/test/java/org/gwtapp/ccalc/client/CCalcTest.java
+++ ccalc/app/src/test/java/org/gwtapp/ccalc/client/CCalcTest.java
@@ -3,10 +3,8 @@ package org.gwtapp.ccalc.client;
 import com.google.gwt.junit.client.GWTTestCase;
 
 public abstract class CCalcTest extends GWTTestCase {
-
 	@Override
 	public String getModuleName() {
 		return "org.gwtapp.ccalc.CCalcTest";
 	}
-
 }
