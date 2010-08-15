169c88d7eb738a2f1b011ffd7380f52243decebc
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-15 15:18:31 +0000

    CCALC-25 User registering service - tests

--- ccalc/app/src/test/java/org/gwtapp/ccalc/client/GwtTestUser.java
+++ ccalc/app/src/test/java/org/gwtapp/ccalc/client/GwtTestUser.java
@@ -2,6 +2,7 @@ package org.gwtapp.ccalc.client;
 
 import org.gwtapp.ccalc.client.data.user.User;
 import org.gwtapp.core.client.SimpleAsyncCallback;
+import org.gwtapp.core.rpc.exception.RpcException;
 import org.junit.Test;
 
 public class GwtTestUser extends CCalcTest {
@@ -12,10 +13,34 @@ public class GwtTestUser extends CCalcTest {
 			@Override
 			public void onSuccess(User user) {
 				assertNotNull(user);
-				assertEquals(new Long(1L),user.getId());
-				assertEquals("aba",user.getLogin());
-				assertEquals("aba@email.com",user.getEmail());
-				assertEquals("Name: aba",user.getName());
+				assertEquals(new Long(1L), user.getId());
+				assertEquals("aba", user.getLogin());
+				assertEquals("aba@email.com", user.getEmail());
+				assertEquals("Name: aba", user.getName());
+				finishTest();
+			}
+		});
+		delayTestFinish(250);
+	}
+
+	@Test
+	public void testUserServiceNullResult() {
+		CCalc.service.getUser("xyz", new SimpleAsyncCallback<User>() {
+			@Override
+			public void onSuccess(User user) {
+				assertNull(user);
+				finishTest();
+			}
+		});
+		delayTestFinish(250);
+	}
+
+	@Test
+	public void testUserServiceRpcException() {
+		CCalc.service.getUser(null, new SimpleAsyncCallback<User>() {
+			@Override
+			public void onFailure(Throwable e) {
+				assertTrue(e instanceof RpcException);
 				finishTest();
 			}
 		});
