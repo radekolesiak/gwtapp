c13443bdb9ef1b309cf41036a0dd784cdeb6fe01
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-17 20:37:44 +0000

    GWTAPP-61 Add 'extension' maven package - 'user' subpackage

---
+++ extension/user/rpc/src/main/java/org/gwtapp/extension/user/client/api/UserService.java
@@ -0,0 +1,15 @@
+package org.gwtapp.extension.user.client.api;
+
+import org.gwtapp.core.rpc.exception.RpcException;
+import org.gwtapp.extension.user.client.data.User;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("extension.user.rpc")
+public interface UserService extends RemoteService {
+
+	User getUser(String login) throws RpcException;
+
+	long addUser(User user) throws RpcException;
+}
--- extension/user/rpc/src/main/java/org/gwtapp/extension/user/client/api/UserServiceAsync.java
+++ extension/user/rpc/src/main/java/org/gwtapp/extension/user/client/api/UserServiceAsync.java
@@ -1,13 +1,10 @@
-package org.gwtapp.ccalc.client.api;
+package org.gwtapp.extension.user.client.api;
 
-import org.gwtapp.ccalc.client.data.book.Book;
 import org.gwtapp.extension.user.client.data.User;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
-public interface CCalcServiceAsync {
-
-	void backup(Book book, AsyncCallback<Void> callback);
+public interface UserServiceAsync {
 
 	void getUser(String login, AsyncCallback<User> callback);
 
