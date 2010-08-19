36a703ccac58ef1e8bf8848572c420fa3d088446
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-19 17:39:48 +0000

    GWTAPP-65 User register by reCaptcha - manual tests

---
+++ startapp/web/src/test/java/org/gwtapp/startapp/client/StartAppManualTestEntryPoint.java
@@ -0,0 +1,10 @@
+package org.gwtapp.startapp.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class StartAppManualTestEntryPoint implements EntryPoint {
+
+	@Override
+	public final void onModuleLoad() {
+	}
+}
