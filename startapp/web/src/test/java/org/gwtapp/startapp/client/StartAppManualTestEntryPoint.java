54b0c9251c2b7116e3f0178cdc8c95428a6dd7d5
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-19 18:04:03 +0000

    GWTAPP-65 User register by reCaptcha - manual tests

--- startapp/web/src/test/java/org/gwtapp/startapp/client/StartAppManualTestEntryPoint.java
+++ startapp/web/src/test/java/org/gwtapp/startapp/client/StartAppManualTestEntryPoint.java
@@ -1,13 +1,31 @@
 package org.gwtapp.startapp.client;
 
+import org.gwtapp.extension.user.client.ReCaptchaUserPanel;
+import org.gwtapp.template.client.TemplateRepository;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class StartAppManualTestEntryPoint implements EntryPoint {
 
+	public final static TemplateRepository template = new TemplateRepository(
+			"/templates/");
+
 	@Override
 	public final void onModuleLoad() {
 		RootPanel.get().add(new Label("GWT Manual Testing"));
+		ReCaptchaUserPanel reCaptcha = new ReCaptchaUserPanel(template
+				.load("user/ReCaptchaUserPanel.jsp"));
+		RootPanel.get().add(reCaptcha);
+		reCaptcha.addChangeHandler(new ChangeHandler() {
+			@Override
+			public void onChange(ChangeEvent event) {
+				Window.alert("CAPTCHA");
+			}
+		});
 	}
 }
---
+++ startapp/web/src/test/war/templates/user/ReCaptchaUserPanel.jsp
@@ -0,0 +1,6 @@
+<t:template styleclass="recaptcha-user-panel">
+<input type="text" t:field="login"></input>
+<input type="text" t:field="email"></input>
+<input type="text" t:field="name"></input>
+<div t:field="reCaptchaContainer"></div>
+</t:template>
