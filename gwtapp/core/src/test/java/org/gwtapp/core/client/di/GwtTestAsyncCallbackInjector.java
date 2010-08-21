0e94040da5534ee5fe4396adb8a2bd7696bfff07
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-21 17:18:03 +0000

    GWTAPP-69 Add Gin Depedency Injections + AsyncCallbackAdapter unit tests

--- gwtapp/core/src/test/java/org/gwtapp/core/client/di/GwtTestAsyncCallbackInjector.java
+++ gwtapp/core/src/test/java/org/gwtapp/core/client/di/GwtTestAsyncCallbackInjector.java
@@ -1,9 +1,11 @@
 package org.gwtapp.core.client.di;
 
 import org.gwtapp.core.client.GWTTestCore;
+import org.gwtapp.core.client.SimpleAsyncCallback;
 import org.junit.Test;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class GwtTestAsyncCallbackInjector extends GWTTestCore {
 
@@ -14,5 +16,22 @@ public class GwtTestAsyncCallbackInjector extends GWTTestCore {
 		SomeClass some = gin.getSomeClass();
 		assertNotNull(some);
 		assertNotNull(some.getInjector());
+	}
+
+	@Test
+	public void testException() {
+		GinjectorService gin = GWT.create(GinjectorService.class);
+		SomeClass some = gin.getSomeClass();
+		AsyncCallback<String> callback = some.getInjector().create(
+				new SimpleAsyncCallback<String>() {
+					@Override
+					public void onFailure(Throwable e) {
+						assertEquals(IllegalStateException.class, e.getClass());
+						finishTest();
+					}
+				});
+		assertNotNull(callback);
+		delayTestFinish(250);
+		callback.onFailure(new IllegalStateException());
 	}
 }
