aa85aeed6fde5058c15a65af23a0505967246cb4
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-23 16:41:44 +0000

    update pom.xml

--- startapp/web/src/test/java/org/gwtapp/startapp/client/ClientModule.java
+++ startapp/web/src/test/java/org/gwtapp/startapp/client/ClientModule.java
@@ -4,6 +4,7 @@ import org.gwtapp.core.client.AsyncCallbackInjector;
 import org.gwtapp.extension.user.client.ReCaptchaUserRegisterServicePanel;
 import org.gwtapp.extension.user.client.data.ReCaptchaUser;
 import org.gwtapp.extension.user.client.data.ReCaptchaUserImpl;
+import org.gwtapp.startapp.client.template.ReCaptchaUserRegisterTemplateCallback;
 import org.gwtapp.template.client.ui.TemplatePanel.TemplateCallback;
 
 import com.google.gwt.inject.client.AbstractGinModule;
@@ -15,7 +16,8 @@ public class ClientModule extends AbstractGinModule {
 		bind(AsyncCallbackInjector.class)
 				.to(AsyncCallbackInjectorAdapter.class);
 		bind(ReCaptchaUser.class).to(ReCaptchaUserImpl.class);
-		bind(TemplateCallback.class).annotatedWith(Names.named(ReCaptchaUserRegisterServicePanel.BIND_NAME)).to(
+		bind(TemplateCallback.class).annotatedWith(
+				Names.named(ReCaptchaUserRegisterServicePanel.BIND_NAME)).to(
 				ReCaptchaUserRegisterTemplateCallback.class);
 	}
 }
