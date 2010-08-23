c5fe0c1532fe25f7d4acddafdab8dfabd9d263ac
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-23 13:18:18 +0000

    GWTAPP-65 User register by reCaptcha - Ginjector binding

--- startapp/web/src/test/java/org/gwtapp/startapp/client/template/ReCaptchaUserRegisterTemplateCallback.java
+++ startapp/web/src/test/java/org/gwtapp/startapp/client/template/ReCaptchaUserRegisterTemplateCallback.java
@@ -1,5 +1,6 @@
-package org.gwtapp.startapp.client;
+package org.gwtapp.startapp.client.template;
 
+import org.gwtapp.startapp.client.StartAppManualTestEntryPoint;
 import org.gwtapp.template.client.callback.TemplateCallbackAdapter;
 
 public class ReCaptchaUserRegisterTemplateCallback extends
