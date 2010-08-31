acbef98856a68af297f418bfef76935ab128ff23
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-08-31 13:01:52 +0000

    * GWTAPP-73
    Clone and upgrade EnumField to ListPanel

--- startapp/web/src/test/java/org/gwtapp/startapp/client/template/ReCaptchaUserRegisterTemplateCallback.java
+++ startapp/web/src/test/java/org/gwtapp/startapp/client/template/ReCaptchaUserRegisterTemplateCallback.java
@@ -3,7 +3,6 @@ package org.gwtapp.startapp.client.template;
 import org.gwtapp.startapp.client.StartAppManualTestEntryPoint;
 import org.gwtapp.template.client.callback.TemplateCallbackAdapter;
 
-// We need use this way as GIN doesn't support 'toInstance' method.
 public class ReCaptchaUserRegisterTemplateCallback extends
 		TemplateCallbackAdapter {
 	public ReCaptchaUserRegisterTemplateCallback() {
