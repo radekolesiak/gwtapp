b15e4831a8a1afe66c7264fc70a68851ce0cfcfa
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-09-14 16:20:26 +0000

    * CCALC-20
    Support international characters on RPC save/download (export/import)

--- ccalc/srv/src/main/java/org/gwtapp/ccalc/server/CCalcDownloadImpl.java
+++ ccalc/srv/src/main/java/org/gwtapp/ccalc/server/CCalcDownloadImpl.java
@@ -1,5 +1,9 @@
 package org.gwtapp.ccalc.server;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.log4j.Logger;
 import org.gwtapp.ccalc.rpc.api.CCalcDownloadService;
@@ -25,8 +29,10 @@ public class CCalcDownloadImpl extends IODownloadHttpServlet implements
 				String fileName = Utils.getFilename(book);
 				getThreadLocalResponse().setContentType(Utils.CONTENT_TYPE);
 				getThreadLocalResponse().setContentLength(content.length());
-				getThreadLocalResponse().setHeader("Content-Disposition",
-						"attachment; filename=\"" + fileName + "\"");
+				getThreadLocalResponse().setHeader(
+						"Content-Disposition",
+						"attachment; filename=\"" + encodeFilename(fileName)
+								+ "\"");
 				getThreadLocalResponse().getOutputStream().print(content);
 				log.debug("Success download");
 			}
@@ -35,4 +41,18 @@ public class CCalcDownloadImpl extends IODownloadHttpServlet implements
 		}
 	}
 
+	private String encodeFilename(String filename)
+			throws UnsupportedEncodingException {
+		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+		String encodedFileName = null;
+		if (userAgent.contains("MSIE") || userAgent.contains("Opera")) {
+			encodedFileName = URLEncoder.encode(filename, "UTF-8");
+		} else {
+			encodedFileName = "=?UTF-8?B?"
+					+ new String(
+							Base64.encodeBase64(filename.getBytes("UTF-8")),
+							"UTF-8") + "?=";
+		}
+		return encodedFileName;
+	}
 }
