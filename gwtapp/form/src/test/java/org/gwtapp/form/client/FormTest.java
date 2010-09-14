8c38ea1a77e9e5560457445be622349ade3ce867
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-09-14 15:58:36 +0000

    * GWTAPP-96
    Update pom.xml files to use testing with modules dependency

--- gwtapp/form/src/test/java/org/gwtapp/form/client/FormTest.java
+++ gwtapp/form/src/test/java/org/gwtapp/form/client/FormTest.java
@@ -3,7 +3,6 @@ package org.gwtapp.form.client;
 import com.google.gwt.junit.client.GWTTestCase;
 
 public abstract class FormTest extends GWTTestCase {
-
 	@Override
 	public String getModuleName() {
 		return "org.gwtapp.form.Form";
--- startapp/web/src/test/java/org/gwtapp/startapp/GwtTestSuiteStartApp.java
+++ startapp/web/src/test/java/org/gwtapp/startapp/GwtTestSuiteStartApp.java
@@ -1,39 +1,46 @@
 package org.gwtapp.startapp;
 
 import junit.framework.Test;
-import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import org.gwtapp.core.client.di.GwtTestAsyncCallbackInjector;
+import org.gwtapp.core.client.ui.GwtTestDelegatedPanel;
+import org.gwtapp.extension.user.client.test.GwtTestUser;
+import org.gwtapp.form.client.ui.GwtTestTemplateModelPanel;
+import org.gwtapp.io.client.GwtTestIOClient;
+import org.gwtapp.template.client.GwtTestHtmlRepository;
+import org.gwtapp.template.client.GwtTestMessages;
+import org.gwtapp.template.client.GwtTestReplaceParameters;
+import org.gwtapp.template.client.GwtTestTemplateRepository;
+import org.gwtapp.template.client.GwtTestUtilsReplaceTemplate;
+import org.gwtapp.template.client.callback.GwtTestTFieldCallback;
+import org.gwtapp.template.client.ui.GwtTestBooleanWrapper;
+import org.gwtapp.template.client.ui.GwtTestTemplateFormPanel;
+import org.gwtapp.template.client.ui.GwtTestTemplatePanel;
+import org.gwtapp.template.client.ui.GwtTestWidgetWrapper;
+import org.gwtapp.template.client.ui.GwtTestWrapperValueCache;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
 
 public class GwtTestSuiteStartApp extends GWTTestSuite {
-
 	public static Test suite() throws ClassNotFoundException {
-		TestSuite suite = new TestSuite("Tests for a StartApp Application");
-		addTest(suite, "org.gwtapp.extension.user.client.test.GwtTestUser");
-		addTest(suite, "org.gwtapp.core.client.ui.GwtTestDelegatedPanel");
-		addTest(suite, "org.gwtapp.core.client.di.GwtTestAsyncCallbackInjector");
-		addTest(suite, "org.gwtapp.template.client.GwtTestReplaceParameters");
-		addTest(suite, "org.gwtapp.template.client.GwtTestUtilsReplaceTemplate");
-		addTest(suite, "org.gwtapp.template.client.GwtTestMessages");
-		addTest(suite, "org.gwtapp.template.client.GwtTestTemplateRepository");
-		addTest(suite, "org.gwtapp.template.client.GwtTestHtmlRepository");
-		addTest(suite, "org.gwtapp.template.client.ui.GwtTestWidgetWrapper");
-		addTest(suite, "org.gwtapp.template.client.ui.GwtTestBooleanWrapper");
-		addTest(suite, "org.gwtapp.template.client.ui.GwtTestTemplatePanel");
-		addTest(suite, "org.gwtapp.template.client.ui.GwtTestTemplateFormPanel");
-		addTest(suite, "org.gwtapp.template.client.ui.GwtTestWrapperValueCache");
-		addTest(suite, "org.gwtapp.template.client.callback.GwtTestTFieldCallback");
-		addTest(suite, "org.gwtapp.form.client.ui.GwtTestTemplateModelPanel");
-		addTest(suite, "org.gwtapp.io.client.GwtTestIOClient");		
+		TestSuite suite = new TestSuite("Tests for GWTApp and StartApp applications");
+		suite.addTestSuite(GwtTestUser.class);
+		suite.addTestSuite(GwtTestDelegatedPanel.class);
+		suite.addTestSuite(GwtTestAsyncCallbackInjector.class);
+		suite.addTestSuite(GwtTestReplaceParameters.class);
+		suite.addTestSuite(GwtTestUtilsReplaceTemplate.class);
+		suite.addTestSuite(GwtTestMessages.class);
+		suite.addTestSuite(GwtTestTemplateRepository.class);
+		suite.addTestSuite(GwtTestHtmlRepository.class);
+		suite.addTestSuite(GwtTestWidgetWrapper.class);
+		suite.addTestSuite(GwtTestBooleanWrapper.class);
+		suite.addTestSuite(GwtTestTemplatePanel.class);
+		suite.addTestSuite(GwtTestTemplateFormPanel.class);
+		suite.addTestSuite(GwtTestWrapperValueCache.class);
+		suite.addTestSuite(GwtTestTFieldCallback.class);
+		suite.addTestSuite(GwtTestTemplateModelPanel.class);
+		suite.addTestSuite(GwtTestIOClient.class);
 		return suite;
-	}
-
-	@SuppressWarnings("unchecked")
-	private static void addTest(TestSuite suite, String c)
-			throws ClassNotFoundException {
-		Class<? extends TestCase> C = (Class<? extends TestCase>) Class
-				.forName(c);
-		suite.addTestSuite(C);
 	}
 }
