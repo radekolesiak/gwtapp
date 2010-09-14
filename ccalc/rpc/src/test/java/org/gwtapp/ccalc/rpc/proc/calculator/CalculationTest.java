244bdbc4d36f6558e144eff311971cc4d7a93443
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-09-14 18:31:41 +0000

    * CCALC-45
    Fix calculator for negative fifo

--- ccalc/rpc/src/test/java/org/gwtapp/ccalc/rpc/proc/calculator/CalculationTest.java
+++ ccalc/rpc/src/test/java/org/gwtapp/ccalc/rpc/proc/calculator/CalculationTest.java
@@ -6,16 +6,36 @@ import java.util.List;
 import org.gwtapp.ccalc.rpc.data.book.Calculation;
 import org.gwtapp.ccalc.rpc.data.book.Currency;
 import org.gwtapp.ccalc.rpc.data.book.Operation;
+import org.gwtapp.ccalc.rpc.data.book.OperationImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculationTest {
 	@Test
-	public void simpleTest() {
+	public void testInitState() {
 		List<Operation> operations = new ArrayList<Operation>();
 		Calculator calculator = new Calculator(Currency.PLN, operations);
 		List<Calculation> calculations = calculator.getCalculations();
 		Assert.assertNotNull(calculations);
 		Assert.assertTrue(calculations.isEmpty());
+	}
+
+	@Test
+	public void testPositiveFifo() {
+		List<Operation> operations = new ArrayList<Operation>();
+		operations.add(getOperation(100.0, 1.0, Currency.USD));
+		operations.add(getOperation(100.0, 2.0, Currency.USD));
+		operations.add(getOperation(-50.0, null, Currency.USD));
+		operations.add(getOperation(-125.0, null, Currency.USD));
+		Calculator calculator = new Calculator(Currency.PLN, operations);
+		List<Calculation> calculations = calculator.getCalculations();
+		Assert.assertNotNull(calculations);
+		Assert.assertEquals(4, calculations.size());
+		{
+		}
+	}
+
+	private Operation getOperation(Double value, Double ratio, Currency currency) {
+		return new OperationImpl(null, null, value, ratio, currency);
 	}
 }
