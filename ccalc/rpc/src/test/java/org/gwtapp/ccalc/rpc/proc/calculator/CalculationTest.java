af0ef3f39e4521b4e2557d1471fb8f8e5a014d7d
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-09-14 18:56:56 +0000

    * CCALC-45
    Fix calculator for negative fifo

--- ccalc/rpc/src/test/java/org/gwtapp/ccalc/rpc/proc/calculator/CalculationTest.java
+++ ccalc/rpc/src/test/java/org/gwtapp/ccalc/rpc/proc/calculator/CalculationTest.java
@@ -61,6 +61,47 @@ public class CalculationTest {
 		}
 	}
 
+	@Test
+	public void testNegativeFifo() {
+		List<Operation> operations = new ArrayList<Operation>();
+		operations.add(getOperation(100.0, 1.0, Currency.USD));
+		operations.add(getOperation(100.0, 7.0, Currency.USD));
+		operations.add(getOperation(-150.0, 4.0, Currency.USD));
+		operations.add(getOperation(-100.0, 4.0, Currency.USD));
+		Calculator calculator = new Calculator(Currency.PLN, operations);
+		List<Calculation> calculations = calculator.getCalculations();
+		Assert.assertNotNull(calculations);
+		Assert.assertEquals(4, calculations.size());
+		{
+			Calculation c = calculations.get(0);
+			Assert.assertEquals(new Double(100.0), c.getIncome());
+			Assert.assertEquals(null, c.getCost());
+			Assert.assertEquals(new Double(0.0), c.getFifo(Currency.USD));
+			Assert.assertEquals(new Double(100.0), c.getFifoBase());
+		}
+		{
+			Calculation c = calculations.get(1);
+			Assert.assertEquals(new Double(700.0), c.getIncome());
+			Assert.assertEquals(null, c.getCost());
+			Assert.assertEquals(new Double(0.0), c.getFifo(Currency.USD));
+			Assert.assertEquals(new Double(700.0), c.getFifoBase());
+		}
+		{
+			Calculation c = calculations.get(2);
+			Assert.assertEquals(null, c.getIncome());
+			Assert.assertEquals(new Double(600.0), c.getCost());
+			Assert.assertEquals(new Double(-3.0), c.getFifo(Currency.USD));
+			Assert.assertEquals(new Double(-450), c.getFifoBase());
+		}
+		{
+			Calculation c = calculations.get(3);
+			Assert.assertEquals(null, c.getIncome());
+			Assert.assertEquals(new Double(400.0), c.getCost());
+			Assert.assertEquals(new Double(-5.5), c.getFifo(Currency.USD));
+			Assert.assertEquals(new Double(-550), c.getFifoBase());
+		}
+	}
+
 	private Operation getOperation(Double value, Double ratio, Currency currency) {
 		return new OperationImpl(null, null, value, ratio, currency);
 	}
