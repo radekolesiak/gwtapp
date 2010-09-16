5174f5d39fca58f10015dd9e7d665f52aa96b035
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-09-16 12:43:12 +0000

    * GWTAPP-97
    Add model pipes

--- gwtapp/core/src/main/java/org/gwtapp/core/client/Pipe.java
+++ gwtapp/core/src/main/java/org/gwtapp/core/client/Pipe.java
@@ -1,11 +1,19 @@
 package org.gwtapp.core.client;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Pipe<T> {
 
 	private final List<PipeHandler<T>> handlers = new ArrayList<PipeHandler<T>>();
+
+	public Pipe() {
+	}
+
+	public Pipe(PipeHandler<T>... handlers) {
+		this.handlers.addAll(Arrays.asList(handlers));
+	}
 
 	public void fireValueChange(T value) {
 		for (PipeHandler<T> handler : handlers) {
