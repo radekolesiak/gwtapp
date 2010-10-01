4f788953d9dd1f983d9bbaade619f1f29e994cc6
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-10-01 13:44:10 +0000

    * GWTAPP-76
    Add unit tests for ListPanel

--- extension/widget/src/test/java/org/gwtapp/extension/widget/client/ui/template/GwtTestWidget.java
+++ extension/widget/src/test/java/org/gwtapp/extension/widget/client/ui/template/GwtTestWidget.java
@@ -1,10 +1,44 @@
 package org.gwtapp.extension.widget.client.ui.template;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.gwtapp.extension.widget.client.WidgetTestCase;
+import org.gwtapp.extension.widget.client.ui.template.ListPanel.DefaultFormatter;
 import org.junit.Test;
 
 public class GwtTestWidget extends WidgetTestCase {
+
+	private static class DoubleFormatter implements ListPanel.Formatter<Double> {
+		@Override
+		public String format(ListPanel<Double> owner, Double item, int index) {
+			if (item == null) {
+				return "";
+			} else {
+				return "" + ((int) item.doubleValue());
+			}
+		}
+	};
+
 	@Test
-	public void test() {
+	public void testInitState() {
+		ListPanel<Double> panel = new ListPanel<Double>();
+		assertNotNull(panel.getFormatter());
+		assertNotNull(panel.getItems());
+		assertNull(panel.getValue());
+		assertTrue(panel.getFormatter() instanceof DefaultFormatter);
+		assertTrue(panel.getItems() instanceof ArrayList);
+		List<Double> items = new ArrayList<Double>();
+		DoubleFormatter formatter = new DoubleFormatter();
+		items.add(1.3);
+		items.add(null);
+		items.add(2.4);
+		items.add(null);
+		items.add(3.5);
+		panel.setItems(items);
+		panel.setFormatter(formatter);
+		assertEquals(items, panel.getItems());
+		assertEquals(formatter, panel.getFormatter());
+		assertNull(panel.getValue());
 	}
 }
