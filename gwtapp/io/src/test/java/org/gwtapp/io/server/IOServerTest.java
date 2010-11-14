3d8c517c0a7c587ab3f4cb06e380f8a189c6fde0
radekolesiak <radekolesiak@31082b6f-3856-0410-ab5b-ccef0860c70c>
2010-11-14 09:31:03 +0000

    * GWTAPP-114
    Cleanup pom.xml files

--- gwtapp/io/src/test/java/org/gwtapp/io/server/IOServerTest.java
+++ gwtapp/io/src/test/java/org/gwtapp/io/server/IOServerTest.java
@@ -17,26 +17,24 @@ public class IOServerTest {
 	@Test
 	public void testSuccessSerializeSerializable() throws IOServerException {
 		Assert.assertEquals(
-				"//OK[2,1,[\"java.lang.String/2004016611\",\"xyz\"],0,6]",
+				"//OK[2,1,[\"java.lang.String/2004016611\",\"xyz\"],0,5]",
 				IOServer.success("xyz"));
 	}
 
 	@Test
 	public void testSuccessSerializeIsSerializable() throws IOServerException {
 		Assert.assertEquals(
-				"//OK[0,0,0,1,[\"org.gwtapp.io.client.Bean/2013084133\"],0,6]",
+				"//OK[0,0,0,1,[\"org.gwtapp.io.client.Bean/2013084133\"],0,5]",
 				IOServer.success(new Bean()));
-		Assert
-				.assertEquals(
-						"//OK[12,0,2,1,[\"org.gwtapp.io.client.Bean/2013084133\",\"aba\"],0,6]",
-						IOServer.success(new Bean("aba", null, 12)));
+		Assert.assertEquals(
+				"//OK[12,0,2,1,[\"org.gwtapp.io.client.Bean/2013084133\",\"aba\"],0,5]",
+				IOServer.success(new Bean("aba", null, 12)));
 	}
 
 	@Test
 	public void testFailureSerialize() throws IOServerException {
-		Assert
-				.assertEquals(
-						"//EX[0,1,[\"org.gwtapp.core.rpc.exception.RpcException/1875557212\"],0,6]",
-						IOServer.failure(new RpcException()));
+		Assert.assertEquals(
+				"//EX[0,1,[\"org.gwtapp.core.rpc.exception.RpcException/1875557212\"],0,5]",
+				IOServer.failure(new RpcException()));
 	}
 }
